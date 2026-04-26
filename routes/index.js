ffb9bdc96b1351390db52268fb20acf93ee48eee
Miroenne <mmenin8306@icloud.com>
2026-04-26 12:27:19 +0200

    Added route to expose README.md file

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const path = require("path");
 
 /**
  * Router handling the root endpoint.
@@ -16,8 +17,12 @@ var router = express.Router();
  * @param {import('express').NextFunction} next - Express next middleware callback.
  * @returns {void} Renders the index view.
  */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Port Russell API" });
+});
+
+router.get("/docs", function (req, res) {
+    res.sendFile(path.join(__dirname, "..", "README.md"));
 });
 
 module.exports = router;
